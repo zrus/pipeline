a524da770ac210fa6cede95d4f37783007d568c5
Tuong Nguyen <tuongn09@gmail.com>
2022-02-16 14:07:43 +0700

    test src link sink

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,154 @@
 // This is used at multiple occassions - for example to iterate an
 // element's pads.
 
+use gst::element_error;
+use gst::glib;
 use gst::prelude::*;
 use gstreamer as gst;
+use gstreamer_app as gst_app;
 
-fn example_main() {
-    gst::init().unwrap();
+use anyhow::Error;
+use byte_slice_cast::*;
+use derive_more::{Display, Error};
 
-    // Create and use an identity element here.
-    // This element does nothing, really. We also never add it to a pipeline.
-    // We just want to iterate the identity element's pads.
-    let identity = gst::ElementFactory::make("identity", None).unwrap();
-    // Get an iterator over all pads of the identity-element.
-    let mut iter = identity.iterate_pads();
-    loop {
-        // In an endless-loop, we use the iterator until we either reach the end
-        // or we hit an error.
-        match iter.next() {
-            Ok(Some(pad)) => println!("Pad: {}", pad.name()),
-            Ok(None) => {
-                // We reached the end of the iterator, there are no more pads
-                println!("Done");
-                break;
+use std::env;
+
+const URI: &'static str = "rtsp://10.50.13.240/1/h264major";
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Missing element {}", _0)]
+struct MissingElement(#[error(not(source))] &'static str);
+
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
+struct ErrorMessage {
+    src: String,
+    error: String,
+    debug: Option<String>,
+    source: glib::Error,
+}
+
+fn create_pipeline() -> Result<gst::Pipeline, Error> {
+    gst::init()?;
+
+    let pipeline = gst::Pipeline::new(None);
+    let src =
+        gst::ElementFactory::make("rtspsrc", None).map_err(|_| MissingElement("audiotestsrc"))?;
+    src.set_property("location", URI);
+    let sink = gst::ElementFactory::make("appsink", None).map_err(|_| MissingElement("appsink"))?;
+
+    pipeline.add_many(&[&src, &sink])?;
+    src.link(&sink)?;
+
+    let appsink = sink
+        .dynamic_cast::<gst_app::AppSink>()
+        .expect("Sink element is expected to be an appsink!");
+
+    // Getting data out of the appsink is done by setting callbacks on it.
+    // The appsink will then call those handlers, as soon as data is available.
+    appsink.set_callbacks(
+        gst_app::AppSinkCallbacks::builder()
+            // Add a handler to the "new-sample" signal.
+            .new_sample(|appsink| {
+                // Pull the sample in question out of the appsink's buffer.
+                let sample = appsink.pull_sample().map_err(|_| gst::FlowError::Eos)?;
+                let buffer = sample.buffer().ok_or_else(|| {
+                    element_error!(
+                        appsink,
+                        gst::ResourceError::Failed,
+                        ("Failed to get buffer from appsink")
+                    );
+
+                    gst::FlowError::Error
+                })?;
+
+                // At this point, buffer is only a reference to an existing memory region somewhere.
+                // When we want to access its content, we have to map it while requesting the required
+                // mode of access (read, read/write).
+                // This type of abstraction is necessary, because the buffer in question might not be
+                // on the machine's main memory itself, but rather in the GPU's memory.
+                // So mapping the buffer makes the underlying memory region accessible to us.
+                // See: https://gstreamer.freedesktop.org/documentation/plugin-development/advanced/allocation.html
+                let map = buffer.map_readable().map_err(|_| {
+                    element_error!(
+                        appsink,
+                        gst::ResourceError::Failed,
+                        ("Failed to map buffer readable")
+                    );
+
+                    gst::FlowError::Error
+                })?;
+
+                // We know what format the data in the memory region has, since we requested
+                // it by setting the appsink's caps. So what we do here is interpret the
+                // memory region we mapped as an array of signed 16 bit integers.
+                let samples = map.as_slice_of::<i16>().map_err(|_| {
+                    element_error!(
+                        appsink,
+                        gst::ResourceError::Failed,
+                        ("Failed to interprete buffer as S16 PCM")
+                    );
+
+                    gst::FlowError::Error
+                })?;
+
+                // For buffer (= chunk of samples), we calculate the root mean square:
+                // (https://en.wikipedia.org/wiki/Root_mean_square)
+                let sum: f64 = samples
+                    .iter()
+                    .map(|sample| {
+                        let f = f64::from(*sample) / f64::from(i16::MAX);
+                        f * f
+                    })
+                    .sum();
+                let rms = (sum / (samples.len() as f64)).sqrt();
+                println!("rms: {}", rms);
+
+                Ok(gst::FlowSuccess::Ok)
+            })
+            .build(),
+    );
+
+    Ok(pipeline)
+}
+
+fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
+    pipeline.set_state(gst::State::Playing)?;
+
+    let bus = pipeline
+        .bus()
+        .expect("Pipeline without bus. Shouldn't happen!");
+
+    for msg in bus.iter_timed(gst::ClockTime::NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                pipeline.set_state(gst::State::Null)?;
+                return Err(ErrorMessage {
+                    src: msg
+                        .src()
+                        .map(|s| String::from(s.path_string()))
+                        .unwrap_or_else(|| String::from("None")),
+                    error: err.error().to_string(),
+                    debug: err.debug(),
+                    source: err.error(),
+                }
+                .into());
             }
-            // It is very important to handle this resync error by calling resync
-            // on the iterator. This error happens, when the container that is iterated
-            // changed during iteration. (e.g. a pad was added while we used the
-            // iterator to iterate over all of an element's pads).
-            // After calling resync on the iterator, iteration will start from the beginning
-            // again. So the application should be able to handle that.
-            Err(gst::IteratorError::Resync) => {
-                println!("Iterator resync");
-                iter.resync();
-            }
-            Err(gst::IteratorError::Error) => {
-                println!("Error");
-                break;
-            }
+            _ => (),
         }
     }
+
+    pipeline.set_state(gst::State::Null)?;
+
+    Ok(())
 }
 
 fn main() {
-    example_main();
+    match create_pipeline().and_then(main_loop) {
+        Ok(r) => r,
+        Err(e) => eprintln!("Error! {}", e),
+    }
 }
